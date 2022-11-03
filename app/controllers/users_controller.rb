68037aed7072a83dbdab80a4f5fde1e1e58e7280
Andrew Stahl <andrewstahl96@gmail.com>
2022-11-03 18:39:26 -0400

    All files working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,38 @@
 class UsersController < ApplicationController
 
   def show
-    user = User.find_by(id: params[:id])
+    user = User.find(params[:id])
     render json: user, include: :items
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "User not found" }, status: :not_found
+  end
+  
+  def items_index
+    user = User.find(params[:user_id])
+    items = user.items 
+    render json: items 
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "User not found" }, status: :not_found
+  end
+  
+  def item_show
+    user = User.find(params[:user_id])
+    item = user.items.find(params[:item_id])
+    render json: item  
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Item not found" }, status: :not_found
+  end
+  
+  def item_create
+    user = User.find(params[:user_id])
+    item = user.items.create(
+      name: params[:name],
+      description: params[:description],
+      price: params[:price]
+    )
+    render json: item, status: :created
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "User not found" }, status: :not_found
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
   resources :users, only: [:show]
+  get '/users/:user_id/items', to: 'users#items_index'
+  get '/users/:user_id/items/:item_id', to: 'users#item_show'
+  post '/users/:user_id/items', to: 'users#item_create'
 end
